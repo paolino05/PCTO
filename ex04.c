dbbba075e772a679297f2d5ebb8cc547e763b107
paolino05 <128369681+paolino05@users.noreply.github.com>
2023-03-20 15:15:50 +0100

    Add files via upload

---
+++ ex04.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+
+{
+
+int a;
+
+printf("insericsci un numero");
+scanf("%d", &a);
+
+a+=7;
+printf("il mio nuovo valore è %d\n", a);
+a-=7;
+printf("il mio nuovo valore è %d\n", a);
+a*=7;
+printf("il mio nuovo valore è %d\n", a);
+a/=7;
+printf("il mio nuovo valore è %d\n", a);
+
+
+}
