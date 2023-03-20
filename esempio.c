58eea41939080c787611d9f4a2824354577e91e0
paolino05 <128369681+paolino05@users.noreply.github.com>
2023-03-20 11:56:22 +0100

    Add files via upload

---
+++ esempio.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int intero = 42; //flag : %d
+    float x = 4.2; //flag : %f
+    double y = 8637253645; //flag : %f
+    char a = 'b'; //flag : %c
+
+printf("ho creato una variabile di tipo int con valore %d\n", intero);
+printf("ho creato una variabile di tipo float con valore %f\n", x);
+printf("ho creato una variabile di tipo double con valore %f\n", y);
+printf("ho creato una variabile di tipo char con valore %c\n", a);
+}
+
