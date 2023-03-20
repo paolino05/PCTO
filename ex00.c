24e8efdac1070283d32e2c8c65ad8167973a9352
paolino05 <128369681+paolino05@users.noreply.github.com>
2023-03-20 12:36:33 +0100

    Add files via upload

---
+++ ex00.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+
+int main()
+{
+float x = 7.8; //flag : %f
+char a = 'c'; //flag : %c
+char b = 'b'; //flag : %c
+
+printf("il primo carattere %c è uguale al secondo carattere %c è uguale al terzo carattere %f\n", a, b, x);
+
+}
