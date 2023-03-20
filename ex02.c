42ab207cf3e459f153b34cbcc462c8ea4c211244
paolino05 <128369681+paolino05@users.noreply.github.com>
2023-03-20 16:11:54 +0100

    Add files via upload

---
+++ ex02.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+int x;
+int y;
+
+printf("inserisci un numero");
+scanf("%d", &x);
+printf("x: %d\n", x);
+printf("inserisci un altro numero");
+scanf("%d", &y);
+printf("y: %d\n", y);
+
+
+
+}
