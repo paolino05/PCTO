b7b6b5aa6a0f6d460a3f2ac93c02b7d0ae3d6423
paolino05 <128369681+paolino05@users.noreply.github.com>
2023-03-22 10:00:35 +0100

    Add files via upload

---
+++ ex17.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+
+{
+    int x;
+    int z;
+    int somma = 0;
+    int media;
+printf("utente inserisce un numero");
+scanf("%d", &x);
+int y = x;
+while(y>0 && y<=x)
+{
+    y--;
+printf("utente inserisce un numero");
+scanf("%d", &z);
+somma = somma + z;
+}
+media = somma/x;
+printf("la somma di tutti i numeri che ho inserito è %d\n", somma);
+printf("la media è %d\n", media);
+}
+
